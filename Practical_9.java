823dbc2c59b563d24f3e47e3894b24beb34de029
ruchitparmar11 <ruchitparmar78@gmail.com>
2024-01-30 17:42:45 +0530

    Add new code

--- Practical_9.java
+++ Practical_9.java
@@ -7,17 +7,16 @@ public class Practical_9 {
             r = r + s.charAt(i);
         }
         if(s.equals(r)) {
-            System.out.println(s + " is Palindrome");
+            System.out.println(s + " is a Palindrome");
         }
         else {
-            System.out.println(s + " is not Palindrome");
+            System.out.println(s + " is not a Palindrome");
         }
     }
     public static void main(String[] args) {
         try (Scanner sc = new Scanner(System.in)) {
             System.out.print("Enter Number or string : ");
             String s = sc.next();
-            System.out.println(s);
             checkPalindrome(s);
         }
     }
