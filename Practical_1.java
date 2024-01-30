05b7db6ea06d8f0f9e8212eac943caed045e297d
ruchitparmar11 <ruchitparmar78@gmail.com>
2024-01-30 15:17:55 +0530

    Add new code

--- Practical_1.java
+++ Practical_1.java
@@ -1,9 +1,9 @@
-import java.util.*;
+import java.util.Scanner;
 
-public class P_01 {
+public class Practical_1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
+
         System.out.println("Enter Values of A B C D E F");
         float A = sc.nextFloat();
         float B = sc.nextFloat();
@@ -11,10 +11,10 @@ public class P_01 {
         float D = sc.nextFloat();
         float E = sc.nextFloat();
         float F = sc.nextFloat();
-        
+
         float x = (((E * D) - (B * F)) / ((A * D) - (B * C))) / E;
         float y = (((A * F) - (E * C)) / ((A * D) - (B * C))) / F;
-        
+
         System.out.println("X =" + x);
         System.out.println("Y =" + y);
     }
