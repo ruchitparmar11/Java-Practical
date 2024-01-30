629e79ad707a31850f6341b335128a0672c59a18
ruchitparmar11 <ruchitparmar78@gmail.com>
2024-01-30 14:59:22 +0530

    Add new code

---
+++ P_01.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class P_01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Enter Values of A B C D E F");
+        float A = sc.nextFloat();
+        float B = sc.nextFloat();
+        float C = sc.nextFloat();
+        float D = sc.nextFloat();
+        float E = sc.nextFloat();
+        float F = sc.nextFloat();
+        
+        float x = (((E * D) - (B * F)) / ((A * D) - (B * C))) / E;
+        float y = (((A * F) - (E * C)) / ((A * D) - (B * C))) / F;
+        
+        System.out.println("X =" + x);
+        System.out.println("Y =" + y);
+    }
+}
