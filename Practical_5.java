bce19f52f9d3dcc1fd84a8406bd9a64f9b7cda61
ruchitparmar11 <ruchitparmar78@gmail.com>
2024-01-30 16:41:10 +0530

    Add new code

---
+++ Practical_5.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Practical_5 {
+    static void max_no(int a, int b, int c) {
+
+        if (a > b) {
+            if (a > c) {
+                System.out.println(a + " is maximum number");
+            } else {
+                System.out.println(c + " is maximum number");
+            }
+        } else if (a < b) {
+            if (b > c) {
+                System.out.println(b + " is maximum number");
+            } else {
+                System.out.println(c + " is maximum number");
+            }
+        } else {
+            System.out.println("All are same");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter Three numbers: ");
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            max_no(a, b, c);
+        }
+    }
+
+}
