96355cfdd931a273d31cd96b236c60a50d7e33c2
ruchitparmar11 <ruchitparmar78@gmail.com>
2024-02-06 13:23:40 +0530

    Add new code

--- Practical_07.java
+++ Practical_07.java
@@ -7,6 +7,8 @@ public class Practical_07 {
             String str = sc.nextLine();
             int length = str.length();
             System.out.println("The size of the String is: " + length);
+            System.out.print("First-Half of the String is: ");
+            System.out.println(str.substring(0, length/2));
             System.out.print("Second-Half of the String is: ");
             System.out.println(str.substring(length/2,length));
         }
